3dafab02439a86b072df4ad36a915bf7dda477de
Kantaporn Sriwichai <iamsayparn@gmail.com>
2022-03-13 15:33:32 +0700

    change all code from html into ejs
    the first one is accidentally click commit

--- server.js
+++ server.js
@@ -8,32 +8,60 @@ app.use(express.static('public'));
 //call ejs template
 app.set('view engine', 'ejs');
 
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get('/login', function(req, res) {
+//     res.sendFile(__dirname + '/login.html');
+// });
+
+// app.get('/table', function(req, res) {
+//     res.sendFile(__dirname + '/table.html');
+// });
+
+// app.get('/sign_up', function(req, res) {
+//     res.sendFile(__dirname + '/sign_up.html');
+// });
+
+// app.get('/menu', function(req, res) {
+//     res.sendFile(__dirname + '/menu.html');
+// });
+
+// app.get('/cart', function(req, res) {
+//     res.sendFile(__dirname + '/cart.html');
+// });
+
+// app.get('/myOrder', function(req, res) {
+//     res.sendFile(__dirname + '/myOrder.html');
+// });
+
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.render('index');
 });
 
 app.get('/login', function(req, res) {
-    res.sendFile(__dirname + '/login.html');
-});
-
-app.get('/table', function(req, res) {
-    res.sendFile(__dirname + '/table.html');
-});
-
-app.get('/sign_up', function(req, res) {
-    res.sendFile(__dirname + '/sign_up.html');
+    res.render('login');
 });
 
 app.get('/menu', function(req, res) {
-    res.sendFile(__dirname + '/menu.html');
+    res.render('menu');
+});
+
+app.get('/table', function(req, res) {
+    res.render('table');
+});
+
+app.get('/sign_up', function(req, res) {
+    res.render('sign_up');
 });
 
 app.get('/cart', function(req, res) {
-    res.sendFile(__dirname + '/cart.html');
+    res.render('cart');
 });
 
 app.get('/myOrder', function(req, res) {
-    res.sendFile(__dirname + '/myOrder.html');
+    res.render('myOrder');
 });
 
 
